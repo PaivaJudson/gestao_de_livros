2982e4938354aa2dd9001554eb3ced711514ff7a
Judson Paiva <judson.paiva@hotmail.com>
2020-09-20 22:16:04 +0100

    Criação da estrutura do projecto

---
+++ src/main/java/com/judsonpaiva/gestaodelivros/GestaodelivrosApplication.java
@@ -0,0 +1,13 @@
+package com.judsonpaiva.gestaodelivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodelivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodelivrosApplication.class, args);
+	}
+
+}
